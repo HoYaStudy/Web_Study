ecc9eafb6b94d972cf920f23c8c1b52584ef1dc0
llHoYall <hoya128@gmail.com>
2020-09-29 21:52:55 +0900

    Deploy Project: Clone Twitter

---
+++ portfolio/CloneTwitter/precache-manifest.a531b6d395e96a78e4c51f03d07c8c90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198c29fb6f9e56f537cc72b7cce93455",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaf63d40fffd90e010fd",
+    "url": "/static/css/main.ac0f328e.chunk.css"
+  },
+  {
+    "revision": "a70933f1f641e1a5ceef",
+    "url": "/static/js/2.b0b7c5e5.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.b0b7c5e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf63d40fffd90e010fd",
+    "url": "/static/js/main.16f59f20.chunk.js"
+  },
+  {
+    "revision": "94d7be087b32ffd44197",
+    "url": "/static/js/runtime-main.3e4ee40c.js"
+  }
+]);
