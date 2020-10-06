7882e28640314c1c1905a6b8a9d606c466dba802
llHoYall <hoya128@gmail.com>
2020-10-06 21:06:50 +0900

    Deploy Project: Firebase

---
+++ portfolio/Firebase/precache-manifest.cc20c9656431f700445a0950b2504224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f907b29ac7ce2e400f6010ab2d952a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13f8f0c9688257dffbaa",
+    "url": "/static/js/2.b2ba6bd9.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.b2ba6bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b56b6572667e22c924eb",
+    "url": "/static/js/main.722cba08.chunk.js"
+  },
+  {
+    "revision": "99d8fc80249f4851f7c1",
+    "url": "/static/js/runtime-main.a7c50f1d.js"
+  }
+]);
